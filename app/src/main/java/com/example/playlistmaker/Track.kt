a1efd73942fa3a2c452643fb6475699ddbac90c4
Roman1233219 <opzadacha@yandex.ru>
2025-12-26 18:25:59 +0300

    домашка спринт 14 - первая попытка

--- app/src/main/java/com/example/playlistmaker/Track.kt
+++ app/src/main/java/com/example/playlistmaker/Track.kt
@@ -11,7 +11,8 @@ data class Track(
    val collectionName: String?,
    val releaseDate: String?,
    val primaryGenreName: String?,
-   val country: String? // Добавляем поле country
+   val country: String?,
+   val previewUrl: String? //  поле для ссылки на  трек
 ) : Serializable {
 
    fun getFormattedTime(): String {
